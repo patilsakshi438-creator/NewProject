c18e66f53f0360f81b67cbebcf13a2a6e4e7859a
patilsakshi438-creator <patilsakshi438@gmail.com>
2026-02-26 16:25:59 +0530

    make changes

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
+import Image from "./pages/Image";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/image" element={<Image />} />
       </Routes>
     </BrowserRouter>
   );
